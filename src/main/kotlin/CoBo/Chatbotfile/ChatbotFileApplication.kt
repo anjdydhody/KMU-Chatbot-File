fce2d2b02759bd4d11472613740fc91aa9c21b53
anjdydhody <a01066444534@gmail.com>
2024-01-08 13:18:42 +0900

    first commit

---
+++ src/main/kotlin/CoBo/Chatbotfile/ChatbotFileApplication.kt
@@ -0,0 +1,11 @@
+package CoBo.Chatbotfile
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChatbotFileApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChatbotFileApplication>(*args)
+}
---
+++ src/test/kotlin/CoBo/Chatbotfile/ChatbotFileApplicationTests.kt
@@ -0,0 +1,13 @@
+package CoBo.Chatbotfile
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChatbotFileApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
