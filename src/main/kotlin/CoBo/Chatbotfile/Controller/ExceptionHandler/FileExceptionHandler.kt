ef88ee0a70bd8b8d1e91004f5b29df5703e26706
trust1204@gmail.com <trust1204@gmail.com>
2024-01-09 22:51:48 +0900

    [feat] add NoSuchElementExceptionHandler

--- src/main/kotlin/CoBo/Chatbotfile/Controller/ExceptionHandler/FileExceptionHandler.kt
+++ src/main/kotlin/CoBo/Chatbotfile/Controller/ExceptionHandler/FileExceptionHandler.kt
@@ -13,4 +13,9 @@ class FileExceptionHandler {
     fun fileIOExceptionHandler():ResponseEntity<String>{
         return ResponseEntity("파일을 업로드하는 과정에서 에러가 발생했습니다.",HttpStatus.SERVICE_UNAVAILABLE)
     }
+
+    @ExceptionHandler(NoSuchElementException::class)
+    fun fileNoSuchElementExceptionHandler(): ResponseEntity<String>{
+        return ResponseEntity("파일을 찾을 수 없음", HttpStatus.NOT_FOUND)
+    }
 }
